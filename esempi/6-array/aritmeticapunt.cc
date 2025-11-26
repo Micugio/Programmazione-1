34876a91d099412ed98af769e6aff28c2612a98d
micugio <giovanni.giovannini@studenti.unitn.it>
2025-11-26 23:23:05 +0100

    Casa 26/11/2025

--- esempi/6-array/aritmeticapunt.cc
+++ esempi/6-array/aritmeticapunt.cc
@@ -11,30 +11,62 @@ int main () {
   cout << "Size(int) = " << sizeof(int) << endl;
   cout << "Address(x) = " << (long) &x << endl;
   cout << "px = " << (long) px << endl;
-  cout << endl;  
+  cout << endl;
 
-  px += 2; // (cioè +8 perchè un sizeof(int)=4 )
-  cout << "px = " << (long) px << endl;  
-  px -= 3;
-  cout << "px = " << (long) px << endl;  
-  px++;
+  px += 2; // (cioè +8 perchè un sizeof(int)=4)
   cout << "px = " << (long) px << endl; 
-  cout << endl;   
-   
+
+  px -= 3; // (cioè -12 , perchè -3*4)
+  cout << "px = " << (long) px << endl;
+
+  px++; // (cioè +4 , perchè +1*4)
+  cout << "px = " << (long) px << endl; 
+
+  cout << endl;
+  cout << "---------------------------------" << endl;
+  cout << endl;
+
   cout << "Size(long double) = " << sizeof(long double) << endl;
   cout << "Address(y) = " << (long) &y << endl;
   cout << "py = " << (long) py << endl;
   cout << endl;
 
-  py += 2;
+  py += 2; // (cioè +32 , perchè +2*12)
   cout << "py = " << (long) py << endl; 
-  pypiudue = py;
-  cout << endl;
- 
-  py -= 3;
+
+  pypiudue = py; // Copia l'indirizzo di py (che è l'indirizzo di y) in pypiudue (NOTA: copia SOLO indirizzo, pypiudue non punta a py).
+  cout << "pypiudue = " << (long) pypiudue << endl;
+
+  /*
+  {
+    RIVEREDERE (vedi domanda chatgpt)
+    double y = 3.5;
+    cout << y << endl;
+
+    
+    double* py = &y;
+    double* pypiudue;   // pypiudue punta a y
+
+    cout << y << endl;
+
+    pypiudue = py;
+  
+
+    *pypiudue = 10.2;
+    
+    cout << y << endl; // stampa 10.2
+    cout << *py << endl; // stampa 10.2
+    cout << *pypiudue << endl; // stampa 10.2
+  }
+  */
+
+  py -= 3; // (cioè -48 , perchè -3*12)
   cout << "py = " << (long) py << endl;  
+  cout << "pypiudue = " << (long) pypiudue << endl; // Valore non cambia perchè pypiudue non punta a py (ho solo l'indirizzo)
+
   py +=1;
-  cout << "py = " << (long) py << endl;  
+  cout << "py = " << (long) py << endl;
+
   cout << endl;
 
   cout << "pypiudue = " << (long) pypiudue << endl; 
