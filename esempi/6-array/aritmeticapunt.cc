f43ce7a4e0fe871d564003f872a8c7a6b48ca3f2
micugio <giovanni.giovannini@studenti.unitn.it>
2025-11-28 18:36:52 +0100

    Casa 28/11/2025

--- esempi/6-array/aritmeticapunt.cc
+++ esempi/6-array/aritmeticapunt.cc
@@ -19,7 +19,7 @@ int main () {
   px -= 3; // (cioè -12 , perchè -3*4)
   cout << "px = " << (long) px << endl;
 
-  px++; // (cioè +4 , perchè +1*4)
+  px++; // (cioè +4 , perchè 1*4)
   cout << "px = " << (long) px << endl; 
 
   cout << endl;
@@ -31,52 +31,52 @@ int main () {
   cout << "py = " << (long) py << endl;
   cout << endl;
 
-  py += 2; // (cioè +32 , perchè +2*12)
+  py += 2; // (cioè +32 , perchè +2*16)
   cout << "py = " << (long) py << endl; 
 
-  pypiudue = py; // Copia l'indirizzo di py (che è l'indirizzo di y) in pypiudue (NOTA: copia SOLO indirizzo, pypiudue non punta a py).
+  pypiudue = py; // il puntatore pypiudue punta a y (perchè il puntatore py contiene l'indirizzo di y), quindi modificando valore di pypiudue modifico anche valore di y.
   cout << "pypiudue = " << (long) pypiudue << endl;
 
-  /*
+
   {
-    RIVEREDERE (vedi domanda chatgpt)
-    double y = 3.5;
-    cout << y << endl;
+    // ESEMPIO di (codice isolato):
+    cout << "-------------------------------" << endl;
 
+    int y = 3;
+    int* py = &y;
+    int* pypiudue;
+
+    cout << "y = " << y << endl;
+
+    pypiudue = py; // pypiudue punta a y (perchè py contiene l'indirizzo di y)
+    *pypiudue = 10; // Quindi se modifico valore di pypiudue modifico anche valore di y
     
-    double* py = &y;
-    double* pypiudue;   // pypiudue punta a y
+    cout << "y = " << y << endl; // stampa 10
+    cout << "py = " << *py << endl; // stampa 10
+    cout << "pypiudue = " << *pypiudue << endl; // stampa 10
 
-    cout << y << endl;
-
-    pypiudue = py;
-  
-
-    *pypiudue = 10.2;
-    
-    cout << y << endl; // stampa 10.2
-    cout << *py << endl; // stampa 10.2
-    cout << *pypiudue << endl; // stampa 10.2
+    cout << "-------------------------------" << endl;
   }
-  */
 
-  py -= 3; // (cioè -48 , perchè -3*12)
+  py -= 3; // (cioè -48 , perchè -3*16)
   cout << "py = " << (long) py << endl;  
   cout << "pypiudue = " << (long) pypiudue << endl; // Valore non cambia perchè pypiudue non punta a py (ho solo l'indirizzo)
 
-  py +=1;
+  py +=1; // (cioè +16 , perchè 1*16)
   cout << "py = " << (long) py << endl;
 
   cout << endl;
 
-  cout << "pypiudue = " << (long) pypiudue << endl; 
-  cout << "py =       " << (long) py << endl;  
-  cout << "pypiudue-py = " << pypiudue-py << endl;
-  cout << "py-pypiudue = " << py-pypiudue << endl;
+  cout << "pypiudue = " << (long) pypiudue << endl; // Valore sempre uguale
+  cout << "py =       " << (long) py << endl;
+  cout << "pypiudue-py (in long) = " << (long) pypiudue - (long) py << endl; // 140723474874144 - 140723474874112 = 32 -> 2*16 -> 2
+  cout << "pypiudue-py (in double) = " << pypiudue - py << endl; // 32 -> 2*16 -> 2
+  cout << "py-pypiudue (in long) = " << (long) py - (long) pypiudue << endl; // 140723474874144 - 140723474874112 = 32 -> 2*16 -> 2
+  cout << "py-pypiudue (in double) = " << py - pypiudue << endl; // 32 -> 2*16 -> 2
   cout << endl;
 
-  cout << "(pypiudue<py) = " << (pypiudue<py) << endl;
-  cout << "(pypiudue>py) = " << (pypiudue>py) << endl;
+  cout << "(pypiudue<py) = " << (pypiudue<py) << endl; // stampa 0 (=false)
+  cout << "(pypiudue>py) = " << (pypiudue>py) << endl; // stampa 1 (=true)
 
   return 0;
 }
