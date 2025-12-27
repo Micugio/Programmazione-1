19e3d33c93a09d6b6759b5e16eed020821c5661a
micugio <giovanni.giovannini@studenti.unitn.it>
2025-12-27 16:50:28 +0100

    Casa 27/12/2025

--- esercizi/ES-RIPASSO/es1-20251203-temp-motori.cc
+++ esercizi/ES-RIPASSO/es1-20251203-temp-motori.cc
@@ -5,19 +5,19 @@ using namespace std;
 
 // ESEGUIRE CON:   ./a.out temperatura1.txt temperatura2.txt
 
-double Percentuale (int superato, int totale);
+double Percentuale (double superato, double totale);
 
 int main (int argc, char * argv[]) {
   fstream f1, f2;
   
-  char ch;
   double t1 = 0.0;
   double t2 = 0.0;
   int superato = 0;
+  int totale = 0;
 
   // RICORDA: argc = numero di elementi (parole) da input.
   if (argc!=3) {
-    cerr << "Usa: ./a.out <fileinput1> <fileinput2> <fileoutput>\n";
+    cerr << "Usa: ./a.out <fileinput1> <fileinput2>\n";
     exit(0);
   }
 
@@ -36,27 +36,40 @@ int main (int argc, char * argv[]) {
     exit(0);
   }
   
-  /*
-  ALTRA VERSIONE MIGLIORE (funziona perchè so che ci sono solo valori double e ogni valore è su una riga senza spazi, QUINDI non serve fare controlli)
-  while(f1 >> v1 && f2 >> v2) {  // Fino a quando leggo da uno o altro, basterebbe anche uno però non farei comando....FINIRE
-      sumv1 += v1;
-      sumv2 += v2;
-      if (v1 > v2)
-          pnum++;
-      num++;
+/*
+  // VERSIONE PROF: 
+  // Funziona perchè nei file ci sono solo valori double, ogni valore è su una riga (cioè diviso da un separatore "a capo") e le righe sono uguali per i file.
+  // QUINDI non serve fare controlli!
+  while(f1 >> t1 && f2 >> t2) {  // Fino a quando leggo da uno o altro, basterebbe anche uno però così non leggerei in loop da entrambi i file.
+    if (t1 > t2) {
+      superato++;
+    }
+    totale++;
   }
 */
 
-  // Prima faccio una lettera iniziale ALTRIMENTI non posso usare eof()
+  // Prima faccio una lettera iniziale ALTRIMENTI non posso usare fail()
   f1 >> t1;
   f2 >> t2;
-  while (!f1.eof()) {  // Basta controllare se il file è terminato (non serve fail() perchè ci sono solo double)
+  while (!f1.fail()) {  // Scelgo di usare fail() perchè se non c'è un separatore dopo l'ultimo elemento letto NON posso USARE eof().
     if (t1 > t2) {
-      ++superato;
+      superato++;
     }
-    f1 >> t1; // continuo a leggere da entrambi i file (NOTA: il puntatore ricorda la posizione in cui è arrivato ad ogni chiamata)
+    totale++;
+    f1 >> t1; // continuo a leggere da entrambi i file in loop (NOTA: il puntatore ricorda la posizione in cui è arrivato ad ogni chiamata)
     f2 >> t2;
   }
+
+  cout << "superato: " << superato << endl;
+  cout << "totale: " << totale << endl;
+
+  if (totale==0) {
+    cout << "Il numero delle misurazioni è uguale a zero, per cui non posso calcolare la percentuale" << endl;
+  }
+  else {
+    cout << "La percentuale di misurazioni in cui la temperatura del motore1 ha superato quella del motore2 è del " << Percentuale (superato, totale) << "%" << endl;
+  }
+  
 
   f1.close();
   f2.close();
@@ -64,10 +77,18 @@ int main (int argc, char * argv[]) {
   return 0;
 }
 
-double Percentuale (int superato, int totale) {
-    double risultato;
+double Percentuale (double superato, double totale) {
+  double risultato;
+  
+  risultato = (superato/totale) * 100;
 
-    risultato = (superato/totale) * 100;
-
-    return risultato;
+  return risultato;
 }
+
+/*
+// VERSIONE PROF:
+double Percentuale (int superato, int totale) {
+
+  return ((double)superato/totale) * 100.0;;
+}
+*/
