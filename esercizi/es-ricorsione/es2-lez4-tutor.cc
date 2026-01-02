2d73675ef665f0d9eacaf4c96ab55c3a5d2f3355
micugio <giovanni.giovannini@studenti.unitn.it>
2026-01-02 23:47:36 +0100

    Casa 02/01/2026

--- esercizi/es-ricorsione/es2-lez4-tutor.cc
+++ esercizi/es-ricorsione/es2-lez4-tutor.cc
@@ -1,33 +1,40 @@
 #include <iostream>
 using namespace std;
 
+
 // CONSEGNA: Stampa in sequenza numeri da 0 a n, prima numeri pari in ordine crescente, poi numeri dispari in ordine decrescente.
 
-void funzione (int i, int n) {
-    
-    if (i>n) {
+
+// NOTA: Con la chiamata ricorsiva la parte sopra viene eseguita in loop (all'andata) fino a quando caso base è vero.
+//       QUANDO il caso base è vero a quel punto si esegue (al ritorno) la parte sotto un numero di volte uguale alla parte sopra, DOPO QUESTO finisce funzione.
+
+void stampa (int n, int i=0) {  // Scrivo (int i=0) così non serve dichiarare (i) all'interno del main modificandolo.
+    if (i > n) {
         return;
     }
     else {
-        if (i%2 == 0) {
-            cout << i << endl;
-            funzione (i+1, n);
+        if (i%2 == 0) {  // Stampa numeri pari all'andata (i va da 0 a n)
+            cout << i << " ";
         }
-        else {
-            funzione (i+1, n);
-            cout << i << endl;
+
+        stampa(n, i+1); // Chiamata ricorsiva
+
+        if (i%2 != 0) {  // Stampa numeri dispari al ritorno (i va da n a 0)
+            cout << i << " ";
         }
     }
 }
 
 int main() {
-    int i = 0;
     int n = 0;
+    
+    do {
+        cout << "Inserisci un numero positivo: " << endl;
+        cin >> n;
+    } while (n<0);
 
-    cout << "Inserisci il numero: ";
-    cin >> n;
-
-    funzione(i, n);
+    stampa(n);
+    cout << endl;
 
     return 0;
 }
